6e93ced1f1492a53eb7a7fa30b0acbc2a9391fd2
Erick González <itenhuck@gmail.com>
2020-08-26 18:56:42 -0500

    cambios 2

---
+++ registro-conocer-sb/src/test/java/com/itok/springboot/app/RegistroConocerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itok.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroConocerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
