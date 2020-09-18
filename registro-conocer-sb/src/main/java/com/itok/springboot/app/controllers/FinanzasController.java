555137380ab0b3c6d5366ca5c90b32a66fe7c8a6
Erick González <itenhuck@gmail.com>
2020-09-18 01:20:09 -0500

    17 septiembre

--- registro-conocer-sb/src/main/java/com/itok/springboot/app/controllers/FinanzasController.java
+++ registro-conocer-sb/src/main/java/com/itok/springboot/app/controllers/FinanzasController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.itok.springboot.app.models.entity.Certificado;
 import com.itok.springboot.app.models.service.ICertificadoService;
 
 
@@ -27,12 +28,39 @@ public class FinanzasController {
 		
 	@GetMapping("asignarPago/{id}")
 	public String asignar(@PathVariable(value="id") int id, Model model, RedirectAttributes flash) {
-		System.out.println("id recibido: " +id);
+		Certificado certificado=null;
 		
-		
-		
-		return  "redirect:/finanzas/notificacionPagos";
+		if (id > 0) {
+			certificado= certificadoCervice.findOne(id);
+			if(certificado ==null) {
+				flash.addFlashAttribute("error", "El ID del certificado no existe en la BBDD!");
+				return "redirect:/finanzas/notificacionPagos";
+			}
+		} else {
+			flash.addFlashAttribute("error", "El ID del certificado no puede ser cero!");
+			return "redirect:/finanzas/notificacionPagos";
+		}
+		model.addAttribute("titulo", "Asignar nuevo pago");
+		model.addAttribute("certificado", certificado);
+		return "/finanzas/nuevoPago";
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
